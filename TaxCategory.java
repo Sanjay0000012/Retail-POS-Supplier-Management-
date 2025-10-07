220688307e991c6ef00895d65f7dda0714437063
Sanjay0000012 <ashokkuar.vas@gmail.com>
2025-10-07 17:41:22 +0530

    Create TaxCategory.java

---
+++ TaxCategory.java
@@ -0,0 +1,9 @@
+package assgn2;
+public class TaxCategory { private String name; private double rate;
+public TaxCategory(String name, double rate) { this.name = name;
+this.rate = rate;
+}
+ 
+public String getName() { return name; } public double getRate() { return rate; }
+}
+
